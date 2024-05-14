d33b8e403a08c38140c1918e2bec4a41e87365bf
hejingzhao <353155692@qq.com>
2024-05-14 10:22:07 +0800

    同步

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 var { createProxyMiddleware } = require("http-proxy-middleware");
 let axios = require("axios");
 const https = require("https");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+let jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 const instance = axios.create({
   httpsAgent: new https.Agent({
@@ -14,132 +16,40 @@ const instance = axios.create({
 });
 
 const options = {
-  key: fs.readFileSync(path.join(__dirname, 'certs', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname,'certs', 'cert.pem'))
+  key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")),
 };
 
 const port = 9996;
+const appId = "9996";
+const secretKey = process.env.SECRET_KEY;
 const app = express();
 https.createServer(options, app).listen(port, () => {
-  console.log(`素数生成器已启动：https://localhost:${port}`);
+  console.log(`魔术游戏已启动：https://localhost:${port}`);
 });
 
-
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(cors());
 
 
+// 检查AccessToken是否有效
 app.get("/checkLogin", (req, res) => {
-  // 通过共享cookie中的token去验证是否登录
-  if (req.cookies["SSO-Cookie"]) {
-    instance
-      .post("https://localhost:3000/cas/checkLogin", {
-        token: req.cookies["SSO-Cookie"],
-      })
-      .then((response) => {
-        if (response.data.code === 1) {
-          res.send({
-            code: 1,
-            msg: "未登录",
-          });
-        }else{
-          res.send({
-            code: 0,
-            data: {
-              msg: "已登录，token有效",
-            },
-          });
-        }
-      })
-      .catch((err) => {
-      });
-  } else {
-    console.log('有cookie')
-    res.send({
-      code: 1,
-      msg: "未登录",
-    });
-  }
-});
-
-app.get("/logout", (req, res) => {
-  instance
-    .get("https://localhost:3000/cas/logout",{
-      headers: {
-        Authorization: req.cookies["SSO-Cookie"],
-      },
-    })
-    .then((response) => {
-      if (response.data.code === 0) {
-        res.cookie("SSO-Cookie", "", { maxAge: 0 });
-        res.send({
-          code: 0,
-          data: {
-            msg: "退出成功",
-          },
-        });
-      }else if(response.data.code === 1){
-        res.send({
-          code: 1,
-          msg: response.data.msg,
-        });
-      }else{
-        res.send({
-          code: 2,
-          msg: response.data.msg,
-        });
-      }
-    })
-    .catch((err) => {
+  let token = req.headers.authorization;
+  jwt.verify(token, secretKey, (err,payload) => {
+    if (err) {
       res.send({
         code: 1,
-        msg: "退出失败",
+        msg: "AccessToken无效"+err,
       });
-    });
-});
-
-app.get("/test", (req, res) => {
-  // 先获取csrfToken
-  instance
-    .get("https://localhost:3000/cas/csrfToken")
-    .then((response) => {
-      let csrfToken = response.data.csrfToken;
-      instance
-        .get("https://localhost:3000/cas/test", {
-          headers: {
-            Authorization: req.cookies["SSO-Cookie"], // 测试按钮是受保护的资源
-            csrftoken:csrfToken
-          },
-        })
-        .then((response) => {
-          if (response.data.code === 0) {
-            res.send({
-              code: 0,
-              data: {
-                msg: response.data.data.msg,
-              },
-            });
-          } else {
-            res.cookie("SSO-Cookie", "", { maxAge: 0 });
-            res.send({
-              code: 1,
-              msg: response.data.msg,
-            });
-          }
-        })
-        .catch((err) => {
-          res.cookie("SSO-Cookie", "", { maxAge: 0 });
-          res.send({
-            code: 1,
-            msg: "测试失败",
-          });
-        });
-    })
-    .catch((err) => {
+    } else {
       res.send({
-        code: 1,
-        msg: "错误+" + err,
+        code: 0,
+        data: {
+          msg: "AccessToken有效，用户已登录",
+        },
       });
-    });
+    }
+  });
 });
--- public/auth.js
+++ public/auth.js
@@ -1,14 +1,25 @@
 let exit = document.querySelector(".exit");
-let testbtn = document.querySelector(".test");
+let normalbtn = document.querySelector(".test1");
+let superbtn = document.querySelector(".test2");
 const appId = "9996";
 
+// 从url中获取参数
+function getFromUrl(id) {
+  const searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.has(id)) {
+    return searchParams.get(id);
+  } else {
+    return "";
+  }
+}
+
 window.onload = function () {
-  // 其它页面跳转到本页面时，会带上授权码
-  if(getFromUrl("authCode")){
+  // 从认证中心跳转到本页面时，会带上授权码
+  if (getFromUrl("authCode")) {
     // 通过授权码获取token
     fetch(`https://localhost:3000/cas/serviceLoginToken?appId=${appId}`, {
       method: "POST",
-      credentials: 'include',
+      credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
@@ -22,78 +33,169 @@ window.onload = function () {
       .then((res) => res.json())
       .then((data) => {
         if (data.code === 0) {
+          console.log("AccessToken", data.data.AccessToken);
+          // 存储token
+          localStorage.setItem(`AccessToken-${appId}`, data.data.AccessToken);
+          localStorage.setItem(`RefreshToken-${appId}`, data.data.RefreshToken);
+          // 刷新页面，去检查是否登录
           window.location.href = `https://localhost:${appId}`;
-          console.log(data.data.msg);
         } else {
           // 授权码无效,重定向到登录页面
           window.location.href = `https://localhost:3000`;
           console.log(data.msg);
         }
       });
-  }else{
-    // 不是其它页面跳转到本页面时，会判断是否登录
+  } else if (
+    localStorage.getItem(`AccessToken-${appId}`) &&
+    localStorage.getItem(`RefreshToken-${appId}`)
+  ) {
+    console.log("本地验证");
     checkLogin();
+  } else {
+    // 不是认证中心跳转到本页面时，且本地应用不存在token，则会判断是否在其他应用登录过
+    checkSSOLogin();
   }
 };
 
 exit.addEventListener("click", function () {
-  console.log("exit");
+  console.log("exit退出");
   logout();
 });
-testbtn.addEventListener("click", function () {
-  console.log("test");
+
+normalbtn.addEventListener("click", function () {
+  console.log("普通用户按钮");
   test();
 });
 
-// 从url中获取参数
-function getFromUrl(id) {
-  const searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.has(id)) {
-    return searchParams.get(id);
-  }else{
-    return '';
+superbtn.addEventListener("click", function () {
+  console.log("超级用户按钮");
+  test();
+});
+
+// 判断用户是否在其他应用登录过
+async function checkSSOLogin() {
+  let res = await fetch(`https://localhost:3000/cas/checkSSOLogin`, {
+    method: "POST",
+    credentials: "include",
+  });
+  let data = await res.json();
+  if (data.code === 0) {
+    // 验证通过，开始单点登录
+    console.log("已在其它应用登录过，开始单点登录", data.data.msg);
+    ssoLogin();
+  } else {
+    // 用户未在其他应用登录过，重定向到认证服务器的登录页面
+    window.location.href = `https://localhost:3000?appId=${appId}`;
   }
 }
 
-// 判断用户是否登录
+// 先本地通过AccessToken检查用户是否登录，若未登录则使用RefreshToken向服务器验证
 async function checkLogin() {
   let res = await fetch(`https://localhost:${appId}/checkLogin`, {
     method: "GET",
+    headers: {
+      Authorization: localStorage.getItem(`AccessToken-${appId}`),
+    },
   });
   let data = await res.json();
   if (data.code === 0) {
-    // token验证通过，已登录：由于各个子应用使用的共享token，就不必单独重新请求了
-    console.log(data.data.msg);
+    console.log("AccessToken有效", data.data.msg);
   } else {
-    // 当用户未登录时，重定向到认证服务器的登录页面
+    // 使用RefreshToken向服务器验证
+    let res = await fetch(`https://localhost:3000/cas/checkLogin`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Authorization: localStorage.getItem(`RefreshToken-${appId}`),
+      },
+    });
+    let result = await res.json();
+    if (result.code === 0) {
+      // RefreshToken有效，更新AccessToken
+      localStorage.setItem(`AccessToken-${appId}`, result.data.AccessToken);
+      console.log("RefreshToken有效", result.data.msg);
+    } else {
+      // RefreshToken失效
+      // todo:这里应该还要重置数据库中的登录状态
+      localStorage.removeItem(`AccessToken-${appId}`);
+      localStorage.removeItem(`RefreshToken-${appId}`);
+      window.location.href = `https://localhost:3000?appId=${appId}`;
+    }
+  }
+}
+
+// 单点登录：获取授权码authCode
+async function ssoLogin() {
+  let res = await fetch(
+    `https://localhost:3000/cas/serviceLoginAuthCode?appId=${appId}`,
+    {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ssoFlag: true,
+      }),
+    }
+  );
+  let result = await res.json();
+  if (result?.code === 0) {
+    // 单点登录成功，跳转回原应用
+    window.location.href = `${result.data.redirectUrl}?authCode=${
+      result.data.authCode
+    }&username=${result.data.username || ""}&phone=${result.data.phone}`;
+  } else {
+    // 单点登录失败，重定向到登录页面
     window.location.href = `https://localhost:3000?appId=${appId}`;
   }
 }
 
+// 退出登录
 async function logout() {
-  let res = await fetch(`https://localhost:${appId}/logout`, {
+  let res = await fetch(`https://localhost:3000/cas/logout`, {
     method: "GET",
+    credentials: "include",
+    headers: {
+      Authorization: localStorage.getItem(`RefreshToken-${appId}`),
+    },
   });
   let data = await res.json();
   if (data.code === 0) {
-    window.location.href = `https://localhost:3000`;
-  } else if(data.code===2){  // 数据库操作失败导致退出失败
-    console.log("退出失败",data.msg)
-  } else{
-    console.log("退出失败",data.msg)
+    // 退出成功
+    localStorage.removeItem(`AccessToken-${appId}`);
+    localStorage.removeItem(`RefreshToken-${appId}`);
+    window.location.href = `https://localhost:3000?appId=${appId}`;
+  } else {
+    console.log("退出失败，已经是未登录状态了，或者是数据库修改报错", data.msg);
+    // 刷新页面，重新检查是否登录
+    window.location.href = `https://localhost:${appId}`;
   }
 }
 
-async function test(){
-  let res = await fetch(`https://localhost:${appId}/test`, {
+// 权限测试按钮
+async function test() {
+  // 先获取csrfToken
+  let res0 = await fetch(`https://localhost:3000/cas/csrfToken`, {
     method: "GET",
+    credentials: "include",
+  });
+  let data0 = await res0.json();
+  let csrfToken = data0.csrfToken;
+  let res = await fetch(`https://localhost:3000/cas/test`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      Authorization: localStorage.getItem(`AccessToken-${appId}`),
+      csrftoken: csrfToken,
+    },
   });
   let data = await res.json();
   if (data.code === 0) {
-    console.log(data.data.msg);
     window.alert(data.data.msg);
-  }else{
-    // 当token失效测试失败时，重定向到认证服务器的登录页面
-    window.location.href = `https://localhost:3000?appId=${appId}`;
+  } else {
+    // 当token失效测试失败时，刷新页面
+    console.log(data.msg);
+    window.location.href = `https://localhost:${appId}`;
   }
 }
